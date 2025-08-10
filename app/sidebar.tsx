293ff8abfc7ac90b53ad16f18917cfb5d05faffb
Cody Thomas Zeitler <codyzeitler12@gmail.com>
2025-08-09 23:53:38 -0700

    feat: make background not blue

--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -2,18 +2,19 @@ import type React from "react";
 import { ZEITLERFORCE_HEADER_HEIGHT } from "./header";
 import { useMediaContext } from "./mediaContext";
 
+import Paper from '@mui/material/Paper';
+
 export function ZeitlerforceSidebar() {
     const { isMobile } = useMediaContext();
 
     const sidebarStyles: React.CSSProperties = {
         position: 'sticky',
         top: ZEITLERFORCE_HEADER_HEIGHT,
-        backgroundColor: 'red',
         width: isMobile ? 10 : 250,
         height: `calc(100vh - ${ZEITLERFORCE_HEADER_HEIGHT}px)`
     };
 
     return (
-        <div style={sidebarStyles}></div>
+        <Paper style={sidebarStyles} elevation={6}></Paper>
     );
 }
