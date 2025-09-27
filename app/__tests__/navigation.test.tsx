dd17d0e22738e4adf77858c0bad686076671d893
Cody Thomas Zeitler <codyzeitler12@gmail.com>
2025-09-26 20:28:10 -0700

    fix: get unit tests up and running

---
+++ app/__tests__/navigation.test.tsx
@@ -0,0 +1,10 @@
+import {describe, expect, it} from '@jest/globals';
+
+describe('<ZeitlerForceNavigation>', () => {
+    it('should do the thing', () => {
+        console.log('This should be a print statement within navigation.test.tsx.');
+    });
+
+    function renderTestObject() {
+    }
+});
---
+++ app/__tests__/root.test.tsx
@@ -0,0 +1,12 @@
+import {describe, expect, it} from '@jest/globals';
+
+
+import { Layout } from '~/root';
+
+describe('<ZeitlerForceDocs>', () => {
+    it('should do the thing', () => {
+    });
+
+    function renderTestObject() {
+    }
+});
