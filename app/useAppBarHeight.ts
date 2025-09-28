eff969d0fce4b8ca7f283367580ad4a8f4784d23
Cody Thomas Zeitler <codyzeitler12@gmail.com>
2025-09-27 19:41:12 -0700

    fix: remove unused window dimensions function

--- app/useAppBarHeight.ts
+++ app/useAppBarHeight.ts
@@ -33,18 +33,3 @@ function getWindowDimensions() {
     height
   };
 }
-
-export function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return windowDimensions;
-}
