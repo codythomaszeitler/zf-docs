9f5b3a6fab307801efd2eff8eb02cbccf74917e7
Cody Thomas Zeitler <codyzeitler12@gmail.com>
2025-09-28 13:28:48 -0700

    fix: finally get react testing library to work

--- app/__tests__/zeitlerForceDocumentation.test.tsx
+++ app/__tests__/zeitlerForceDocumentation.test.tsx
@@ -1,11 +1,8 @@
-
 import { describe, it, expect } from '@jest/globals';
-import { ZeitlerForceDocumentation, ZeitlerForceDocumentationContext, type ZeitlerForceDocumentationContextType, type ZeitlerForceDocumentationProps } from '../zeitlerForceDocumentation';
-import { render, isMobileMediaQuery, screen, isDesktopMediaQuery } from './testUtil';
-import { createRoutesStub, MemoryRouter } from 'react-router';
+import { type ZeitlerForceDocumentationContextType } from '../zeitlerForceDocumentation';
+import { isMobileMediaQuery, screen, isDesktopMediaQuery, renderAppTestObject } from './testUtil';
 import { act } from 'react';
-import { URL } from 'url';
-import { waitFor } from '@testing-library/dom';
+
 
 describe('<ZeitlerForceDocumentation>', () => {
 
@@ -38,31 +35,21 @@ describe('<ZeitlerForceDocumentation>', () => {
             expect(tree).toBeDefined();
         });
 
-        it('should "deploy on save" when you select deployments -> deploy on save', () => {
+        it('should "deploy on save" when you select deployments -> deploy on save', async () => {
             renderTestObject(isDesktopMediaQuery());
 
-            clickOnMenuItems('Deployments', 'Deploy on Save');
+            await clickOnMenuItems('Deployments', 'Deploy on Save');
+            const deployOnSaveScreen = screen.queryByTestId('deploy-on-save-screen');
+            expect(deployOnSaveScreen).toBeTruthy();
         });
     });
 
     it('should navigation back to dashboard ', async () => {
-
-        const Stub = createRoutesStub([
-            {
-                path : 'deploy-on-save',
-                
-            }
-        ])
-
         renderTestObject(isDesktopMediaQuery());
         await clickOnMenuItems('Deployments', 'Deploy on Save');
 
-        await waitFor(() => {
-            const url = new URL(window.location.href);
-            expect(url.pathname).not.toBe('/')
-        });
-
-        console.log(window.location.href);
+        const deployOnSaveScreen = screen.queryByTestId('deploy-on-save-screen');
+        expect(deployOnSaveScreen).toBeTruthy();
     });
 
     async function clickOnMenuItems(...labels: string[]) {
@@ -79,11 +66,6 @@ describe('<ZeitlerForceDocumentation>', () => {
     }
 
     function renderTestObject(context: ZeitlerForceDocumentationContextType) {
-        return render(
-            <ZeitlerForceDocumentationContext.Provider value={context}>
-                <MemoryRouter>
-                    <ZeitlerForceDocumentation></ZeitlerForceDocumentation>
-                </MemoryRouter>
-            </ZeitlerForceDocumentationContext.Provider>);
+        return renderAppTestObject(context);
     }
 });
--- app/browserRouter.ts
+++ app/browserRouter.ts
@@ -4,16 +4,17 @@ import {
 import { ZeitlerForceDocumentation } from "./zeitlerForceDocumentation";
 import DeployOnSaveDocumentation from "./routes/deployOnSaveDocumentation";
 
-// Okay, so what in the world is going wrong here?
+export const routerConfig = [
+    {
+        path: "/",
+        Component: ZeitlerForceDocumentation,
+    },
+    {
+        path: 'deploy-on-save',
+        Component: DeployOnSaveDocumentation
+    }
+];
+
 export function genBrowserRouter() {
-    return createBrowserRouter([
-        {
-            path: "/",
-            Component: ZeitlerForceDocumentation,
-        },
-        {
-            path: 'deploy-on-save',
-            Component: DeployOnSaveDocumentation
-        }
-    ]);
+    return createBrowserRouter(routerConfig);
 }
