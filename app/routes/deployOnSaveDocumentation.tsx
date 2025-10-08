45dc273b344492d75172cd9179cb8d121ba84789
Cody Thomas Zeitler <codyzeitler12@gmail.com>
2025-10-07 19:28:33 -0700

    fix: start to make generic text boxes for docs

--- app/routes/deployOnSaveDocumentation.tsx
+++ app/routes/deployOnSaveDocumentation.tsx
@@ -1,32 +1,63 @@
-import { Typography } from "@mui/material";
+import { Typography, type TypographyOwnProps } from "@mui/material";
 import Box from "@mui/material/Box";
 
 import Divider from '@mui/material/Divider';
+import React from "react";
+
+/*
+    We are going to have sections on the right side that correlate to the 
+    right side. 
+
+    We are trying to find some way to consistently apply the padding around a line.
+
+
+    For example - should there be a react element that does this 
+    or should it be a class style that is applied.
+*/
 
 export default function DeployOnSaveDocumentation() {
     return (
         <Box data-testid='deploy-on-save-screen'>
-            <Typography variant="h4">Deployments</Typography>
+            <DocumentationHeader>
+                Deployments
+            </DocumentationHeader>
             <Divider></Divider>
-            <Typography variant="body1" sx={{
-                py: 1
-            }}>
+            <DocumentationBody>
                 This is our explanation of how deployments work.
                 This is going to be a very, very, very long message.
-            </Typography>
-            <Divider sx={{
-                paddingBottom: 1
-            }}></Divider>
-            <Typography variant="h5" sx={{
-                paddingTop: 1
-            }}>Deployment Queues</Typography>
-            <Divider sx={{
-                paddingBottom: 1
-            }}></Divider>
+            </DocumentationBody>
+            <Divider></Divider>
+            <DocumentationSubHeader>
+                Deploy on Save
+            </DocumentationSubHeader>
+            <Divider></Divider>
         </Box>
     );
 }
 
-function BodyMessage() {
+type Variant = Pick<TypographyOwnProps, "variant">;
+type DocumentationProps = { children?: React.ReactNode[] | React.ReactNode } & Variant
 
+function DocumentationHeader({ children, variant = 'h4' }: DocumentationProps) {
+    return (<Typography variant={variant}> {children}</Typography >);
+}
+
+function DocumentationSubHeader({ children, variant = 'h5' }: DocumentationProps) {
+    return (<Typography variant={variant}> {children}</Typography >);
+}
+
+function DocumentationBody({ children }: { children?: React.ReactNode[] | React.ReactNode }) {
+    return (<Typography variant="body2" sx={{
+        py: 1
+    }}>
+        {children}
+    </Typography>);
+}
+
+function LineDivider() {
+    return (
+        <Divider sx={{
+            marginBottom: 10
+        }}></Divider>
+    );
 }
